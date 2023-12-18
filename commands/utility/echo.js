962fa546991cf089fe3ecdf334f0679243050a7c
Niara <123399425+Niaraa@users.noreply.github.com>
2023-12-17 20:00:35 -0500

    echo.js success

--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -1,29 +1,15 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-const { SlashCommandBuilder, ChannelType } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
-const data = new SlashCommandBuilder()
-  .setName('echo')
-  .setDescription('Replies with your input!')
-  // eslint-disable-next-line arrow-parens
-  .addStringOption(option =>
-    // eslint-disable-next-line implicit-arrow-linebreak
-    option.setName('input')
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('Replies with your input!')
+    // eslint-disable-next-line arrow-parens
+    .addStringOption((option) => option.setName('input')
       .setDescription('The input to echo back')
-      .addChannelTypes(discord.js))
-  // eslint-disable-next-line arrow-parens
-  .addChannelOption(option =>
-    // eslint-disable-next-line implicit-arrow-linebreak
-    option.setName('channel')
-      .setDescription('The channel to echo into')
-      .addChannelTypes(ChannelType.GuildText))
-  .addBooleanOption(option =>
-    option.setName('embed')
-      .setDescription('Whether or not the echo should be embedded'));
-      	.setRequired(true)
-  .addChoices(
-    { name: 'Funny' },
-    { name: 'Cartoon' },
-    { name: 'Movie' }
-    // eslint-disable-next-line eol-last
-  );
+      .setRequired(true)
+      .setMaxLength(25)),
+  async execute(interaction) {
+    await interaction.reply({ content: interaction.options.getString('input') });
+  },
+};
